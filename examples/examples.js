6c5080f9a29349247d7ae86e1bac0aa7ae4308c3
anderslatif <anderslatif@gmail.com>
2023-12-15 14:11:13 +0100

    Added examples

---
+++ examples/examples.js
@@ -0,0 +1,12 @@
+import KeysKey from "../lib/index.js";
+// import KeysKey from "keyskey";
+
+);
+// console.log(await ks.sendKey('a'));
+// console.log(await ks.sendKeys(['a', 'b']));
+// console.log(await ks.sendCombination(['control', 'shift', 'v']));
+
+
+// const event3 = { key: 'A', metaKey: true, shiftKey: true, ctrlKey: false };
+// const result3 = KeysKey.And(event3, KeysKey.SpecialGroups.MetaAndShift(event3));
+// console.log("**********", result3);
